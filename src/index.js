790a96bf59810646666d74144b2bba8c0795c055
Lorenzoo <olnv22@gmail.com>
2022-07-10 18:16:22 -0400

    update 2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mysqlConnection = require("./database");
 const cors = require("cors");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const path = require("path");
 
 app.use(cors());
